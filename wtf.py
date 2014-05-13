077b64c862640a974c18d8812a63ed5e202e37da
tezro <a@andreyshipilov.com>
2014-05-14 00:40:47 +0930

    pep8, pylint

--- wtf.py
+++ wtf.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python
+# pylint: disable=E1101
+
+"""
+    "What The Frequency"
+    https://soundcloud.com/what_the_frequency
+    Every time our music room is filled with sounds and tunes,
+    30 seconds is being randomly recorded and posted to SoundCloud once a day.
+"""
+
 from __future__ import print_function
 from datetime import datetime
 from os.path import abspath, join, dirname
@@ -15,7 +24,7 @@ DEBUG = True if 'debug' in sys.argv else False
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
-RATE = 44100
+RATE = 48000
 SHORT_NORMALIZE = 1.0/32768.0
 
 # Minimum treshold (RMS) to reach to start the record.
@@ -29,7 +38,7 @@ RECORD_DELAY_TO = 5 if DEBUG else 5
 RECORD_DELAY_PERIOD = 0.1 if DEBUG else 60
 
 # How many seconds to record.
-RECORD_LENGTH = 30 if DEBUG else 30
+RECORD_LENGTH = 1 if DEBUG else 30
 
 # Colors for RMS terminal output.
 RMS_COLORS = {
@@ -42,11 +51,16 @@ RMS_COLORS = {
 # Directory to save audio files in.
 AUDIO_DIRECTORY = join(abspath(dirname(__file__)), "audio")
 
+
 class AudioProcessingException(Exception):
+    """Fake exception class."""
+
     pass
 
 
 class WTF(object):
+    """Main class"""
+
     def __init__(self):
         """Create PyAudio instance. Open audio stream."""
         self.pyaudio_obj = pyaudio.PyAudio()
@@ -62,60 +76,57 @@ class WTF(object):
         self.latest_wav_file_path = self.latest_mp3_file_path = ""
 
     def run(self):
+        """Main method"""
         if DEBUG:
             print(Fore.RED + "We're in debug mode btw!")
 
         print("Let's start. [{0}]\n".format(datetime.now()))
 
-        try:
-            while True:
-                try:
-                    input = self.stream.read(CHUNK)
-                except IOError:
-                    input = '\x00' * CHUNK
-                rms_value = self.get_rms(input)
-                self.draw_eq(rms_value, THRESHOLD)
+        while True:
+            try:
+                input_data = self.stream.read(CHUNK)
+            except IOError:
+                input_data = '\x00' * CHUNK
+            rms_value = self.get_rms(input_data)
+            self.draw_eq(rms_value, THRESHOLD)
 
-                if rms_value > THRESHOLD:
-                    import time
-                    from random import randrange
+            if rms_value > THRESHOLD:
+                import time
+                from random import randrange
 
-                    print(Fore.GREEN + "\nOk, loud as hell! [{1}]".format(
-                        THRESHOLD, datetime.now()))
-                    timeout_minutes = randrange(RECORD_DELAY_FROM,
-                                                RECORD_DELAY_TO)
-                    print("You guys keep playing, " +
-                          "I'll wait for {0} minutes.".format(timeout_minutes))
+                print(Fore.GREEN + "\nOk, loud as hell! [{1}]".format(
+                    THRESHOLD, datetime.now()))
+                timeout_minutes = randrange(RECORD_DELAY_FROM,
+                                            RECORD_DELAY_TO)
+                print("You guys keep playing, " +
+                      "I'll wait for {0} minutes.".format(timeout_minutes))
 
-                    for minute in xrange(0, timeout_minutes):
-                        print("{0}. ".format(minute + 1), end="")
-                        time.sleep(RECORD_DELAY_PERIOD)
+                for minute in xrange(0, timeout_minutes):
+                    print("{0}. ".format(minute + 1), end="")
+                    time.sleep(RECORD_DELAY_PERIOD)
 
-                    print("\nYep, let's record that.[{0}]".format(
-                        datetime.now()))
+                print("\nYep, let's record that.[{0}]".format(
+                    datetime.now()))
 
-                    audio_data = self.record_audio(RECORD_LENGTH)
-                    self.terminate_stream()
+                audio_data = self.record_audio(RECORD_LENGTH)
+                self.terminate_stream()
 
-                    print("I'll try to save it. [{0}]".format(datetime.now()))
-                    self.save_audio(audio_data)
+                print("I'll try to save it. [{0}]".format(datetime.now()))
+                self.save_audio(audio_data)
 
-                    print("Let's convert the WAV to MP3. [{0}]".\
-                          format(datetime.now()))
-                    self.convert_wav_to_mp3()
+                print("Let's convert the WAV to MP3. [{0}]".
+                      format(datetime.now()))
+                self.convert_wav_to_mp3()
 
-                    print("Uploading the masterpiece to SoundColud. [{0}]".\
-                          format(datetime.now()))
-                    self.upload_to_soundcloud()
+                print("Uploading the masterpiece to SoundColud. [{0}]".
+                      format(datetime.now()))
+                self.upload_to_soundcloud()
 
-                    print("\n" + "_" * 79)
-        except Exception as e:
-            print(e)
+                print("\n" + "_" * 79)
 
     @staticmethod
     def draw_eq(rms, maximum):
         """Draws textual equalizer. A bit flaky but, meh."""
-
         level = (rms * 79 / maximum)
         color = RMS_COLORS.get(str(int(level * 3 / 79)), '4')
         # Yes, the next line draws a penis.
@@ -132,8 +143,8 @@ class WTF(object):
         shorts = struct.unpack("%dh" % (count), frame)
         sum_squares = 0.0
         for sample in shorts:
-            n = sample * SHORT_NORMALIZE
-            sum_squares += n * n
+            num = sample * SHORT_NORMALIZE
+            sum_squares += num * num
         rms = pow(sum_squares/count, 0.5)
         return int(rms * 1000)
 
@@ -142,7 +153,7 @@ class WTF(object):
         seconds = int(seconds)
         if seconds:
             frames = []
-            for i in xrange(0, int(RATE / CHUNK * seconds)):
+            for _ in xrange(0, int(RATE / CHUNK * seconds)):
                 try:
                     frames.append(self.stream.read(CHUNK))
                 except IOError:
@@ -185,17 +196,27 @@ class WTF(object):
         """Converts WAV file to MP3."""
         import subprocess
         from os.path import splitext, exists
+        from os import remove
 
-        self.latest_mp3_file_path = splitext(self.latest_wav_file_path)[0] + ".mp3"
+        self.latest_mp3_file_path = "{0}.mp3".format(
+            splitext(self.latest_wav_file_path)[0])
         if not exists(self.latest_wav_file_path):
-            raise AudioProcessingException, "file %s does not exist" % self.latest_wav_file_path
+            raise AudioProcessingException(
+                "file {0} does not exist".format(self.latest_wav_file_path))
 
-        command = ["lame", "--silent", "--abr", str(75), self.latest_wav_file_path, self.latest_mp3_file_path]
-        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        (stdout, stderr) = process.communicate()
+        command = ["lame",
+                   "--silent",
+                   "--abr", str(75),
+                   self.latest_wav_file_path,
+                   self.latest_mp3_file_path]
+        process = subprocess.Popen(command, stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE)
+        (stdout, _) = process.communicate()
 
         if process.returncode != 0 or not exists(self.latest_mp3_file_path):
-            raise AudioProcessingException, stdout
+            raise AudioProcessingException(stdout)
+        else:
+            remove(self.latest_wav_file_path)
 
     def upload_to_soundcloud(self):
         """Uploads MP3 to SoundCloud."""
@@ -216,10 +237,6 @@ class WTF(object):
             'tag_list': 'live demo jam',
             'downloadable': True
         })
-
-    def post_to_twitter(self):
-        """TODO: Creates a Twitter post with a SoundCloud link."""
-        pass
 
     def terminate_stream(self):
         """Stop the stream, terminate PyAudio."""
